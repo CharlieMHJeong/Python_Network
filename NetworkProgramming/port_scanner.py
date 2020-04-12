7bbdd8c362b20db5c47151fefc0a2309652df188
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2020-04-12 13:50:24 +1000

    Update port_scanner.py

--- NetworkProgramming/port_scanner.py
+++ NetworkProgramming/port_scanner.py
@@ -1,41 +1,63 @@
 #!/bin/python3
+
 import sys
 import socket
+from termcolor import colored
 from datetime import datetime
+import logging
 
-#Define target
-if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
-else:
-    print("Invalid amount of args. ")
-    print("Syntax: python3 scanner.py <ip>")
+logger = logging.getLogger('Port Scan Application CJ')
+logger.setLevel(logging.DEBUG)
 
+#create file handler with logs even debug messages
+fh = logging.FileHandler('ip_port_log.txt')
+fh.setLevel(logging.DEBUG)
 
-#Add a Banner
-print("-" * 50)
-print("Scanning target " + target)
-print("Time Started: " + str(datetime.now()))
-print("-" * 50)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
 
-try:
-    for port in range(50,85): #(startingport, endingport)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+def port_scanner(target, port):
+    """
+    Check port status
+    :param target: IP Address as str
+    :param port:  Port number as int
+    :return:
+    """
+
+    try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.1)
         result = s.connect_ex((target,port)) #returns an error indicator
         #print("Checking port {}".format(port))
         if result == 0:
-            print("Port {} is open".format(port))
+            # print(colored("\tPort {} : Open".format(port), attrs=['bold']))
+            logger.info("\tPort {} : Open".format(port))
+        else:
+            # print("\tPort {} : Closed".format(port))
+            logger.info("\tPort {} : Closed".format(port))
         s.close()
 
-except KeyboardInterrupt:
-    print("\nExting Program!!")
-    sys.exit()
+    except KeyboardInterrupt:
+        # print("\nExting Program!!")
+        logger.info("\nExting Program!!")
+        sys.exit()
 
-except socket.gaierror:
-    print("Hostname could not be resolved")
-    sys.exit()
+    except socket.gaierror:
+        # print("Hostname could not be resolved")
+        logger.info("Hostname could not be resolved")
+        sys.exit()
 
-except socket.error:
-    print("Could not connect to the server")
-    sys.exit()
+    except socket.error:
+        # print("Could not connect to the server")
+        logger.info("Could not connect to the server")
+        sys.exit()
